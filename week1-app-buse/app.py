5e913119da18456ad3e0690089c46dc11aeba775
zbuseaydin <zeynpaydinn@gmail.com>
2024-09-17 22:49:48 +0300

    implement basic app

---
+++ week1-app-buse/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+import os
+from langchain_cohere import ChatCohere
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+os.environ["COHERE_API_KEY"] = "zkHV3A9cXrN8oEtd7eW8dhwx2IQuKwmo4O21tGoN"
+model = ChatCohere(model="command-r-plus")
+parser = StrOutputParser()
+
+system_template = "Determine the language of the following text and translate it into {language}. Give the output in this format: <language_of_the_text> -- <translation>"
+user_template = "Text:{text}"
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", user_template)]
+)
+chain = prompt_template | model | parser
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send_message', methods=['POST'])
+def generate_response():
+    language = request.form["language"]
+    text = request.form["text"]
+
+    response = chain.invoke({"language": language, "text": text})
+    response = response.split("--")
+    response_lang = response[0]
+    response_translation = response[1]
+    return render_template('index.html', text=text, language=response_lang, translation=response_translation, translated_lang=language)
+
+if __name__ == '__main__':
+    app.run(debug=True)
